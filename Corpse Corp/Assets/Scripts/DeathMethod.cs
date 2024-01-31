cbf271f957e8f7c482a4fe9db2ddb050ff2e937a
Sam Peterson (RIT Student) <sap2481@rit.edu>
2024-01-31 10:38:22 -0500

    Death Method start

--- Corpse Corp/Assets/Scripts/DeathMethod.cs
+++ Corpse Corp/Assets/Scripts/DeathMethod.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class DeathMethod : MonoBehaviour
 {
-    public string Name;
-    public string Description;
-    public Sprite Icon;
+    //==== FIELDS ====
+    public string name;
+    public string description;
+    public Sprite icon;
+
+    public float rateOfSale;
+    public float price;
+
+    //==== CONSTRUCTOR ====
+    public DeathMethod(string name, string description, Sprite icon, float rateOfSale, float price)
+    {
+        this.name = name;
+        this.description = description;
+        this.icon = icon;
+        this.rateOfSale = rateOfSale;
+        this.price = price;
+    }
+    public DeathMethod(string name)
+    {
+        this.name = name;
+    }
 
     // Start is called before the first frame update
     void Start()
