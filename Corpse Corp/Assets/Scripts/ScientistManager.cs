15bd345531c4442ad6b6ea428e39e2d649f40978
James Pettibone (RIT Student) <jdp9029@rit.edu>
2024-02-02 10:38:19 -0500

    Combos completed

--- Corpse Corp/Assets/Scripts/ScientistManager.cs
+++ Corpse Corp/Assets/Scripts/ScientistManager.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ScientistManager : MonoBehaviour
 {
-    Scientist[] scientists;
+    public Scientist[] scientists;
+
 
     // Start is called before the first frame update
     void Start()
@@ -48,6 +49,102 @@ public class ScientistManager : MonoBehaviour
         scientists[0].ComboScientist(scientists[8], new DeathMethod("Gang Activity"));
         scientists[0].ComboScientist(scientists[11], new DeathMethod("Drowning"));
         scientists[0].ComboScientist(scientists[22], new DeathMethod("Broken Glass"));
+
+        //hs graduate
+        scientists[1].ComboScientist(scientists[2], new DeathMethod("Hit By Bus"));
+        scientists[1].ComboScientist(scientists[5], new DeathMethod("Scissors"));
+        scientists[1].ComboScientist(scientists[6], new DeathMethod("Scaffolding Collapse"));
+        scientists[1].ComboScientist(scientists[22], new DeathMethod("Chemical Burn"));
+
+        //college student
+        scientists[2].ComboScientist(scientists[8], new DeathMethod("Extremism"));
+        scientists[2].ComboScientist(scientists[13], new DeathMethod("Cocaine"));
+
+        //vet
+        scientists[3].ComboScientist(scientists[4], new DeathMethod("E. Coli"));
+        scientists[3].ComboScientist(scientists[10], new DeathMethod("Anthrax"));
+        scientists[3].ComboScientist(scientists[11], new DeathMethod("Shark Attack"));
+        scientists[3].ComboScientist(scientists[17], new DeathMethod("Smallpox"));
+
+        //chef
+        scientists[4].ComboScientist(scientists[7], new DeathMethod("Kitchen Fire"));
+        scientists[4].ComboScientist(scientists[8], new DeathMethod("Famine"));
+        scientists[4].ComboScientist(scientists[10], new DeathMethod("Salmonella"));
+        scientists[4].ComboScientist(scientists[13], new DeathMethod("Hemlock"));
+        scientists[4].ComboScientist(scientists[17], new DeathMethod("Obesity"));
+        scientists[4].ComboScientist(scientists[22], new DeathMethod("Poison"));
+
+        //barber
+        scientists[5].ComboScientist(scientists[9], new DeathMethod("Scalping"));
+        scientists[5].ComboScientist(scientists[12], new DeathMethod("Electric Razor"));
+
+        //construction worker
+        scientists[6].ComboScientist(scientists[7], new DeathMethod("Jackhammer"));
+        scientists[6].ComboScientist(scientists[12], new DeathMethod("Chainsaw"));
+
+        //mechanic
+        scientists[7].ComboScientist(scientists[12], new DeathMethod("Busted Engine"));
+        scientists[7].ComboScientist(scientists[14], new DeathMethod("Faulty Airlock"));
+        scientists[7].ComboScientist(scientists[20], new DeathMethod("Head-on Collision"));
+        scientists[7].ComboScientist(scientists[22], new DeathMethod("Exhaust Inhalation"));
+
+        //historian
+        scientists[8].ComboScientist(scientists[10], new DeathMethod("Plague"));
+        scientists[8].ComboScientist(scientists[12], new DeathMethod("Shipwreck"));
+        scientists[8].ComboScientist(scientists[14], new DeathMethod("Big Bang"));
+        scientists[8].ComboScientist(scientists[17], new DeathMethod("Black Death"));
+        scientists[8].ComboScientist(scientists[19], new DeathMethod("Fireworks"));
+        scientists[8].ComboScientist(scientists[22], new DeathMethod("Mustard Gas"));
+
+        //gov't agent
+        scientists[9].ComboScientist(scientists[11], new DeathMethod("Waterboarding"));
+        scientists[9].ComboScientist(scientists[13], new DeathMethod("Ricin"));
+        scientists[9].ComboScientist(scientists[16], new DeathMethod("Climate Change"));
+        scientists[9].ComboScientist(scientists[18], new DeathMethod("The Football"));
+
+        //biologist
+        scientists[10].ComboScientist(scientists[11], new DeathMethod("Animal Attack"));
+        scientists[10].ComboScientist(scientists[12], new DeathMethod("Submarine Implosion"));
+
+        //marine biologist
+        scientists[11].ComboScientist(scientists[14], new DeathMethod("Asphyxiation"));
+        scientists[11].ComboScientist(scientists[15], new DeathMethod("Flood"));
+        scientists[11].ComboScientist(scientists[16], new DeathMethod("Tsunami"));
+        scientists[11].ComboScientist(scientists[19], new DeathMethod("Torpedo"));
+
+        //engineer
+        scientists[12].ComboScientist(scientists[20], new DeathMethod("Lab Explosion"));
+        scientists[12].ComboScientist(scientists[22], new DeathMethod("Poison Grenade"));
+
+        //botanist
+
+        //astronomer
+        scientists[14].ComboScientist(scientists[16], new DeathMethod("Meteorite"));
+        scientists[14].ComboScientist(scientists[18], new DeathMethod("Supernova"));
+        scientists[14].ComboScientist(scientists[19], new DeathMethod("Stranded in Space"));
+
+        //meteorologist
+        scientists[15].ComboScientist(scientists[16], new DeathMethod("Drought"));
+        scientists[15].ComboScientist(scientists[22], new DeathMethod("Acid Rain"));
+
+        //geologist
+        scientists[16].ComboScientist(scientists[21], new DeathMethod("Uranium"));
+        scientists[16].ComboScientist(scientists[22], new DeathMethod("Volcano"));
+
+        //epidemiologist
+        scientists[17].ComboScientist(scientists[21], new DeathMethod("Cancer"));
+
+        //nuclear scientist
+        scientists[18].ComboScientist(scientists[20], new DeathMethod("Atom Bomb"));
+        scientists[18].ComboScientist(scientists[21], new DeathMethod("Fallout"));
+
+        //rocket scientist
+
+        //physicist
+
+        //radiologist
+
+        //chemist
 
         #endregion
 
