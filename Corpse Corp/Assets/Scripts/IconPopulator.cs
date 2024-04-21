e30db62c5fab5c69b4f5eb20b8c7ed3fb09a579a
jdp9029 <jdp9029@g.rit.edu>
2024-04-21 16:41:44 -0400

    pushing cuz im boutta break the whole thing

--- Corpse Corp/Assets/Scripts/IconPopulator.cs
+++ Corpse Corp/Assets/Scripts/IconPopulator.cs
@@ -9,21 +9,63 @@ public class IconPopulator : MonoBehaviour
     [SerializeField] GameObject sciIconHolderPrefab;
     [SerializeField] GameObject iconPrefab;
     [SerializeField] GameObject purchasedScientistParent;
-    [HideInInspector] int purchasedScientists = 0;
 
     List<Scientist> registeredPurchasedScientists = new List<Scientist>();
 
     // Start is called before the first frame update
     void Start()
     {
+        //start by instantiating three scroll thingies
         
+        Instantiate(sciIconHolderPrefab, transform);
+        Instantiate(sciIconHolderPrefab, transform);
+        Instantiate(sciIconHolderPrefab, transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(purchasedScientists != purchasedScientistParent.transform.childCount)
+        if(registeredPurchasedScientists.Count != purchasedScientistParent.transform.childCount)
         {
+            //if we need are populating the original three scrollviews
+            if(purchasedScientistParent.transform.childCount < 12)
+            {
+                //figure out the new scientists
+                for (int i = 0; i < purchasedScientistParent.transform.childCount; i++)
+                {
+                    Scientist sci = purchasedScientistParent.transform.GetChild(i).GetComponent<Scientist>();
+
+                    //if we come across a new scientist, handle it accordingly
+                    if (!registeredPurchasedScientists.Contains(sci))
+                    {
+                        registeredPurchasedScientists.Add(sci);
+
+                        int child = 0;
+
+                        if(registeredPurchasedScientists.Count <= 4)
+                        {
+                            child = 0;
+                        }
+                        else if(registeredPurchasedScientists.Count <= 8)
+                        {
+                            child = 1;
+                        }   
+                        else
+                        {
+                            child = 2;
+                        }
+
+                        GameObject newIconAsset = Instantiate(iconPrefab, transform.GetChild(child).GetChild(0).GetChild(0));
+                        newIconAsset.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = sci.name;
+                        newIconAsset.transform.GetChild(1).GetComponent<Image>().sprite = sci.Icon;
+                    }
+                }
+
+
+                //don't do the rest of the code
+                return;
+            }    
+
             //figure out the new scientists
             for(int i = 0; i < purchasedScientistParent.transform.childCount ; i++)
             {
@@ -32,11 +74,10 @@ public class IconPopulator : MonoBehaviour
                 //if we come across a new scientist, handle it accordingly
                 if(!registeredPurchasedScientists.Contains(sci))
                 {
-                    purchasedScientists++;
                     registeredPurchasedScientists.Add(sci);
 
                     //if we do not need a new holder
-                    if (Mathf.Ceil((float)(purchasedScientists - 1) / 4f) == Mathf.Ceil(((float)purchasedScientists / 4f)))
+                    if (Mathf.Ceil((float)(registeredPurchasedScientists.Count - 1) / 4f) == Mathf.Ceil(((float)registeredPurchasedScientists.Count / 4f)))
                     {
                         RectTransform lastHolderContent = transform.GetChild(transform.childCount - 1).GetChild(0).GetChild(0).GetComponent<RectTransform>();
 
